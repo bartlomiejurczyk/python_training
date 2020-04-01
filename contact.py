d174dda577a5f039e06e160aba31cf39d667acd5
Bartek Jurczyk <bartek4833@wp.pl>
2020-04-01 17:22:05 +0200

    Homework No1 (3)

---
+++ contact.py
@@ -0,0 +1,33 @@
+__author__ = 'bartek'
+
+
+class Contact:
+
+    def __init__(self, _first_name, _middle_name, _last_name, _nickname, _tittle, _company, _address,
+                 _telephone_home, _telephone_mobile, _telephone_work, _fax,
+                 _email, _email_2, _email_3, _homepage, _bday, _bmonth, _byear, _aday, _amonth, _ayear,
+                 _secondary_address, _secondary_home, _secondary_notes):
+        self.first_name = _first_name
+        self.middle_name = _middle_name
+        self.last_name = _last_name
+        self.nickname = _nickname
+        self.tittle = _tittle
+        self.company = _company
+        self.address = _address
+        self.telephone_home = _telephone_home
+        self.telephone_mobile = _telephone_mobile
+        self.telephone_work = _telephone_work
+        self.fax = _fax
+        self.email = _email
+        self.email2 = _email_2
+        self.email3 = _email_3
+        self.homepage = _homepage
+        self.bday = _bday
+        self.bmonth = _bmonth
+        self.byear = _byear
+        self.aday = _aday
+        self.amonth = _amonth
+        self.ayear = _ayear
+        self.secondary_address = _secondary_address
+        self.secondary_home = _secondary_home
+        self.secondary_notes = _secondary_notes
