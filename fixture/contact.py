9a7a23b092e3b1cc43911fb918136193bacb203f
Bartek Jurczyk <bartek4833@wp.pl>
2020-04-05 12:58:04 +0200

    Homework 2

---
+++ fixture/contact.py
@@ -0,0 +1,84 @@
+from selenium.webdriver.support.select import Select
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def fill_new_entry(self, contact):
+        wd = self.app.wd
+        self.add_new_entry()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.tittle)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.telephone_home)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.telephone_mobile)
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.telephone_work)
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact.email3)
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_xpath("//option[@value='" + contact.bday + "']").click()
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_xpath("//option[@value='" + contact.bmonth + "']").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_name("aday").click()
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
+        wd.find_element_by_xpath("(//option[@value='" + contact.aday + "'])[2]").click()
+        wd.find_element_by_name("amonth").click()
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
+        wd.find_element_by_xpath("(//option[@value='" + contact.amonth + "'])[2]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(contact.ayear)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.secondary_address)
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact.secondary_home)
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.secondary_notes)
+        # add new entry (press to submit button)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_to_homepage()
+
+
+    def add_new_entry(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+
+
+    def return_to_homepage(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
---
+++ test/add_test_entry.py
@@ -0,0 +1,39 @@
+
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from model.contact import Contact
+from fixture.application import Application
+import pytest
+
+
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_empty_entry(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.fill_new_entry(Contact(_first_name="", _middle_name="", _last_name="", _nickname="",
+                    _tittle="", _company="", _address="", _telephone_home="",
+                    _telephone_mobile="", _telephone_work="", _fax="", _email="",
+                    _email_2="", _email_3="", _homepage="", _bday="", _bmonth="-",
+                    _byear="", _aday="", _amonth="-", _ayear="", _secondary_address="",
+                    _secondary_home="", _secondary_notes=""))
+    app.session.logout()
+
+
+def test_untitled_test_case(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.fill_new_entry(Contact(_first_name="Zbigniew", _middle_name="Janusz", _last_name="Brzeczyszczykiewicz", _nickname="nick123",
+                          _tittle="tittle123", _company="Company XYZ", _address="adress 3/5", _telephone_home="658123123",
+                          _telephone_mobile="654987987", _telephone_work="54121212", _fax="+52 45878787", _email="email123@email.com",
+                          _email_2="email2@email2.com", _email_3="email3@email3.com", _homepage="www.bartek123.com", _bday="10", _bmonth="July",
+                          _byear="1989", _aday="19", _amonth="December", _ayear="2010", _secondary_address="secondary address",
+                          _secondary_home="secondaryHome", _secondary_notes="some additional notes"))
+    app.session.logout()
+
+
+
+
