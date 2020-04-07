545df1d656fd51c72de25c25a4c59d20d7fec783
Bartek Jurczyk <bartek4833@wp.pl>
2020-04-07 11:23:40 +0200

    Homework No6

--- test/add_test_entry.py
+++ test/add_test_entry.py
@@ -1,16 +1,6 @@
 
 # -*- coding: utf-8 -*-
-from selenium import webdriver
 from model.contact import Contact
-from fixture.application import Application
-import pytest
-
-
-@pytest.fixture()
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_empty_entry(app):
