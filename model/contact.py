4967f009729ce77ac2ec289f525fcdce32deeee7
Bartek Jurczyk <bartek4833@wp.pl>
2020-04-22 21:58:29 +0200

    Homework No12v2

--- model/contact.py
+++ model/contact.py
@@ -34,7 +34,6 @@ class Contact:
         self.secondary_notes = _secondary_notes
         self.id = _id
 
-
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name and self.last_name == other.last_name
 
--- test/test_update_contact.py
+++ test/test_update_contact.py
@@ -26,7 +26,7 @@ def test_update_contact(app):
                 _secondary_home="EDIT", _secondary_notes="EDIT notes")
     contact.id = old_contacts[0].id
     app.contact.update_first_contact(contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
